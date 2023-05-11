93cf8d1e973222386595e58ac1eb5113d6a0c020
biyuhuaping <biyuhuaping@163.com>
2023-05-11 16:59:36 +0800

    no message

--- Example/ZBBasicUtilsDemo/ViewController.swift
+++ Example/ZBBasicUtilsDemo/ViewController.swift
@@ -13,28 +13,14 @@ import BaiduMapAPI_Map
 class ViewController: UIViewController {
     
     lazy var searchGeo = XYYLocationReverseGeo()
-
+    @IBOutlet weak var myLab: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        let button1 = UIButton()
-        button1.setTitle("单次精确定位", for: .normal)
-        button1.backgroundColor = UIColor.gray
-        button1.frame = CGRect(x: 80, y: 260, width: 220, height: 40)
-        button1.addTarget(self, action: #selector(button1Action), for: .touchUpInside)
-        self.view.addSubview(button1)
-        
-        let button2 = UIButton()
-        button2.setTitle("根据地理坐标获取地址信息", for: .normal)
-        button2.backgroundColor = UIColor.gray
-        button2.frame = CGRect(x: 80, y: 340, width: 240, height: 40)
-        button2.addTarget(self, action: #selector(button2Action), for: .touchUpInside)
-        self.view.addSubview(button2)
     }
     
-    @objc private func button1Action() {
+    @IBAction func btn1Action(_ sender: Any) {
         /*
         XYYLocationManager.share.requestSingleLocation(true) { (locationModel) in
             print("定位经纬度 ", locationModel.location as Any)
@@ -43,20 +29,24 @@ class ViewController: UIViewController {
         }
         */
         XYYLocationManager.share.requestFullAccuracySingleLocation { (locationModel) in
-            print("定位经纬度 ", locationModel.location as Any)
-            print("详细位置信息", locationModel.address as Any)
-            print("错误信息", locationModel.error as Any)
+            let str1 = locationModel.location as Any
+            let str2 = (locationModel.address ?? "") as String
+            let str3 = locationModel.error as Any
+            self.myLab.text = "定位经纬度:\(str1)\n详细位置信息:\(str2)\n错误信息:\(str3)"
         }
     }
     
-    @objc private func button2Action() {
+    @IBAction func btn2Action(_ sender: Any) {
         self.searchGeo.reverseGeocode(CLLocationCoordinate2DMake(40.00179, 116.487017)) { (locationModel) in
-            print("1定位经纬度 ", locationModel.location as Any)
-            print("1详细位置信息", locationModel.address as Any)
-            print("1错误信息", locationModel.error as Any)
-            print("1POI信息", locationModel.addressComponet?.poiList as Any)
+            let str1 = locationModel.location as Any
+            let str2 = (locationModel.address ?? "") as String
+            let str3 = locationModel.error as Any
+            let str4 = locationModel.addressComponet?.poiList as Any
+
+            self.myLab.text = "1---\n定位经纬度:\(str1)\n详细位置信息:\(str2)\n错误信息:\(str3)\nPOI信息:\(str4)"
         }
     }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let viewController = LocationViewController()
         self.present(viewController, animated: true, completion: nil)
