11489f90ef4074f2b4e54544acf8b2a6ac160f56
biyuhuaping <biyuhuaping@163.com>
2023-05-11 14:32:38 +0800

    去掉无用参数

--- Example/ZBBasicUtilsDemo/ViewController.swift
+++ Example/ZBBasicUtilsDemo/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import ZBBasicUtilsDemo
+import BaiduMapAPI_Map
 
 class ViewController: UIViewController {
+    
+    lazy var searchGeo = XYYLocationReverseGeo()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let button1 = UIButton()
+        button1.setTitle("单次精确定位", for: .normal)
+        button1.backgroundColor = UIColor.gray
+        button1.frame = CGRect(x: 80, y: 260, width: 220, height: 40)
+        button1.addTarget(self, action: #selector(button1Action), for: .touchUpInside)
+        self.view.addSubview(button1)
+        
+        let button2 = UIButton()
+        button2.setTitle("根据地理坐标获取地址信息", for: .normal)
+        button2.backgroundColor = UIColor.gray
+        button2.frame = CGRect(x: 80, y: 340, width: 240, height: 40)
+        button2.addTarget(self, action: #selector(button2Action), for: .touchUpInside)
+        self.view.addSubview(button2)
     }
-
+    
+    @objc private func button1Action() {
+        /*
+        XYYLocationManager.share.requestSingleLocation(true) { (locationModel) in
+            print("定位经纬度 ", locationModel.location as Any)
+            print("详细位置信息", locationModel.address as Any)
+            print("错误信息", locationModel.error as Any)
+        }
+        */
+        XYYLocationManager.share.requestFullAccuracySingleLocation { (locationModel) in
+            print("定位经纬度 ", locationModel.location as Any)
+            print("详细位置信息", locationModel.address as Any)
+            print("错误信息", locationModel.error as Any)
+        }
+    }
+    
+    @objc private func button2Action() {
+        self.searchGeo.reverseGeocode(CLLocationCoordinate2DMake(40.00179, 116.487017)) { (locationModel) in
+            print("1定位经纬度 ", locationModel.location as Any)
+            print("1详细位置信息", locationModel.address as Any)
+            print("1错误信息", locationModel.error as Any)
+            print("1POI信息", locationModel.addressComponet?.poiList as Any)
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let viewController = LocationViewController()
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- ZBBasicUtilsDemo.podspec
+++ ZBBasicUtilsDemo.podspec
@@ -28,9 +28,12 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/biyuhuaping/ZBBasicUtilsDemo.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '10.0'
-
+  s.ios.deployment_target = '9.0'
   s.source_files = 'ZBBasicUtilsDemo/Classes/**/*'
+  s.swift_version = '5.0'
+  s.static_framework = true
+  s.dependency 'BaiduMapKit', '~> 6.5.7'
+  s.dependency 'BMKLocationKit', '~> 2.0.7'
   
   # s.resource_bundles = {
   #   'ZBBasicUtilsDemo' => ['ZBBasicUtilsDemo/Assets/*.png']
@@ -39,4 +42,41 @@ TODO: Add long description of the pod here.
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+
+  
+#  s.subspec 'BMKLocationKit' do |kit|
+#    kit.preserve_paths = 'ZBBasicUtilsDemo/Vendors/*.framework'
+#    kit.vendored_frameworks = 'ZBBasicUtilsDemo/Vendors/*.framework'
+#    kit.libraries = 'sqlite3.0','c++','z'
+#    kit.frameworks = 'CoreLocation','SystemConfiguration','Security','CoreTelephony','AdSupport','CoreGraphics','OpenGLES','QuartzCore'
+#    kit.pod_target_xcconfig = {
+#        'OTHER_LDFLAGS' => '-ObjC',
+#        'LD_RUNPATH_SEARCH_PATHS' => '$(PODS_ROOT)/ZBBasicUtilsDemo/Vendors',
+#        'HEADER_SEARCH_PATHS' => '$(PODS_ROOT)/ZBBasicUtilsDemo/Vendors/*.framework'
+#    }
+#  end
+#
+#  s.prepare_command = <<-EOF
+#    #创建定位Module
+#    rm -rf ZBBasicUtilsDemo/Vendors/BMKLocationKit.framework/Modules
+#    mkdir ZBBasicUtilsDemo/Vendors/BMKLocationKit.framework/Modules
+#    touch ZBBasicUtilsDemo/Vendors/BMKLocationKit.framework/Modules/module.modulemap
+#    cat <<-EOF > ZBBasicUtilsDemo/Vendors/BMKLocationKit.framework/Modules/module.modulemap
+#    framework module BMKLocationKit {
+#      umbrella header "BMKLocationComponent.h"
+#      export *
+#    }
+#    \EOF
+#  EOF
+#
+#  s.subspec 'Location' do |location|
+#      location.source_files = 'ZBBasicUtilsDemo/Location/**/*'
+#      location.dependency 'ZBBasicUtilsDemo/BMKLocationKit'
+#      location.dependency 'ZBBasicUtilsDemo/Permossion'
+#  end
+#
+#  s.subspec 'Permossion' do |permossion|
+#      permossion.source_files = 'ZBBasicUtilsDemo/Permission/**/*'
+#  end
+  
 end
